3bece96e55989c38d46e103b951680c3f7ac1f7d
sne-syn <snezhana.syngur@gmail.com>
2021-08-28 14:50:03 +0200

    Add form validation

--- script.js
+++ script.js
@@ -23,8 +23,43 @@ showModalButton.addEventListener('click', showModal);
 closeModalButton.addEventListener('click', closeModal);
 window.addEventListener('click', (evt) => evt.target === modal ? closeModal() : false);
 window.addEventListener('keydown', (evt) => {
-  if (evt.keyCode === ESC_KEY) {
+  const key = evt.key || evt.keyCode;
+  if (key === 'Escape' || key === 'Esc' || key === ESC_KEY) {
     evt.preventDefault();
     closeModal();
   }
 });
+
+// Validate Form
+const validateUrl = (nameValue, urlValue) => {
+  const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+
+  if (!nameValue || !urlValue) {
+    alert('Please submit values for both fields');
+    return false;
+  }
+
+  if (!urlValue.match(regex)) {
+    alert('Please provide a valid web address');
+    return false;
+  }
+
+  // Valid
+  return true;
+}
+
+const storeBookmark = (evt) => {
+  evt.preventDefault();
+  const nameValue = websiteNameElement.value;
+  let urlValue = websiteUrlElement.value;
+  if (!urlValue.includes('http://') || !urlValue.includes('https://')) {
+    urlValue = `https://${urlValue}`;
+  }
+  
+  if (!validateUrl(nameValue, urlValue)) {
+    return false;
+  }
+}
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
